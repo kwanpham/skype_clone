88ce6afdb23f8c01e367e242298faaa8b462341b
quandev <chomchomcapital@gmail.com>
2021-03-09 15:37:16 +0700

    done part 7

--- lib/screens/search_screan.dart
+++ lib/screens/search_screan.dart
@@ -7,6 +7,8 @@ import 'package:skype_clone/resources/firebase_repository.dart';
 import 'package:skype_clone/utils/universal_variables.dart';
 import 'package:skype_clone/widget/custom_tile.dart';
 
+import 'chatscreens/chat_screen.dart';
+
 class SearchScreen extends StatefulWidget {
   @override
   _SearchScreenState createState() => _SearchScreenState();
@@ -111,7 +113,14 @@ class _SearchScreenState extends State<SearchScreen> {
 
         return CustomTile(
           mini: false,
-          onTap: () {},
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ChatScreen(
+                      receiver: searchedUser,
+                    )));
+          },
           leading: CircleAvatar(
             backgroundImage: NetworkImage(searchedUser.profilePhoto),
             backgroundColor: Colors.grey,
